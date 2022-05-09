61fe40444e09fc85dc8f94d07aba396011efc97d
Carroll <1186684149@qq.com>
2022-05-09 15:45:49 +0800

    [FIX]图片优化

--- src/layout/Subscriber/index.tsx
+++ src/layout/Subscriber/index.tsx
@@ -3,12 +3,12 @@
  * @version: 
  * @Author: Carroll
  * @Date: 2022-03-06 14:18:30
- * @LastEditTime: 2022-03-14 17:54:56
+ * @LastEditTime: 2022-05-09 15:44:37
  */
 
 import { defineComponent, renderSlot } from "vue";
 import Container from "../Container";
-import bg1 from "@/assets/images/bg1.png";
+import bg1 from "@/assets/images/bg1.jpg";
 export default defineComponent({
     name: "Subscriber",
     render() {
