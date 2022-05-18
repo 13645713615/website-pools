94332aa820528fef177242be301d8e7458d7e79d
Carroll <1186684149@qq.com>
2022-05-18 18:30:52 +0800

    [IMP]子账户限制20

--- src/views/Account/layout/Create/index.tsx
+++ src/views/Account/layout/Create/index.tsx
@@ -3,7 +3,7 @@
  * @version: 
  * @Author: Carroll
  * @Date: 2022-03-22 17:39:50
- * @LastEditTime: 2022-05-09 17:41:05
+ * @LastEditTime: 2022-05-18 18:30:35
  */
 
 import ModalForm, { AlertProps } from "@/components/ModalForm";
@@ -81,7 +81,7 @@ const CreateFormButton = defineComponent({
                 })
               
                 if (!isExistAccount) account.push(key);
-                if (account.length >= 5) {
+                if (account.length >= 20) {
                     notice(account)
                     return true
                 }
