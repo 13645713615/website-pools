edddf39f3381c2820c34ade874ec8ec6441d905a
Carroll <1186684149@qq.com>
2022-05-20 14:00:11 +0800

    Revert "[IFX]收益调整"
    This reverts commit e665d811096d36d8c34e9432b8356d61e7f3c5c5.

--- src/views/Home/layout/Table/option.tsx
+++ src/views/Home/layout/Table/option.tsx
@@ -3,7 +3,7 @@
  * @version: 
  * @Author: Carroll
  * @Date: 2022-03-03 13:15:49
- * @LastEditTime: 2022-05-20 13:40:59
+ * @LastEditTime: 2022-04-29 13:52:28
  */
 
 import { useApp } from "@/store";
